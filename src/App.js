1b87310336a1785b944bc5ebc58c5512fc7dbcd2
yousef <yousefsherif7500@gmail.com>
2022-10-07 13:07:59 +0200

    ReactCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react'
 
-export default App;
+
+import MapSection from './components/map/Map' // import the map here
+
+
+
+const location = {
+  address: '1600 Amphitheatre Parkway, Mountain View, california.',
+  lat: 37.42216,
+  lng: -122.08427,
+} // our location object from earlier
+
+const App = () => (
+  <div className="App">
+    <MapSection location={location} zoomLevel={17} /> {/* include it here */}
+  </div>
+)
+
+export default App
